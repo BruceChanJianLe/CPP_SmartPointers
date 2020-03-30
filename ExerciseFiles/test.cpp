10a91ff470a1367128af3b2f7d775999b5bbf347
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-03-31 06:33:20 +0800

    udpate

--- ExerciseFiles/test.cpp
+++ ExerciseFiles/test.cpp
@@ -1,3 +1,59 @@
 #include <iostream>
 #include <string>
-#include "strc.h"
+#include <memory>
+#include "my_strc.hpp"
+
+using namespace std;
+
+
+void message(const string s)
+{
+    cout << endl << s << endl << flush;
+}
+
+
+void disp(std::unique_ptr<myStrC> & o)
+{
+    if(o)
+    {
+        cout << o->value() << endl;
+    }
+    else
+    {
+        cout << "null" << endl;
+    }
+    cout << flush;
+}
+
+
+int main(int argc, char ** argv)
+{
+    message("create unique pointer one");
+    std::unique_ptr<myStrC> a(new myStrC("one"));
+    disp(a);
+
+    message("make_unique two");
+    auto b = std::make_unique<myStrC>("two");
+    disp(a);
+    disp(b);
+
+    message("reset a to three");
+    a.reset(new myStrC("three"));
+    disp(a);
+    disp(b);
+
+    message("move b to c");
+    auto c = std::move(b);
+    disp(a);
+    disp(b);
+    disp(c);
+
+    message("reset a");
+    a.reset();
+    disp(a);
+    disp(b);
+    disp(c);
+
+    message("end of scope");
+    return 0;
+}
